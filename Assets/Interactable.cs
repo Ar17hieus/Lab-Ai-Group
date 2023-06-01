8bdf7565ddbd880fd1a00b24e1d65095d880ce9f
Hazim <B032110058@student.utem.edu.my>
2023-06-02 01:36:30 +0800

    Week 9

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -14,7 +14,7 @@ public class Interactable : MonoBehaviour
     public virtual void Interact()
     {
         //This is meant to be overwritten
-        Debug.Log("INTERACTING WITH" + transform.name);
+        //Debug.Log("Interacting with " + transform.name);
         
     }
 
